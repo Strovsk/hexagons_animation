c9ba5873d850e26821b82228ceb50c6a6935d3e6
Thiago Santa Clara <strovsk@outlook.com>
2024-02-17 02:37:05 -0300

    Update CSS and HTML files with code context

--- public/js/how.js
+++ public/js/how.js
@@ -1,5 +1,5 @@
-function getMessages() {
-  const msgElm = document.getElementById("messages");
+function getMessages(id = "messages") {
+  const msgElm = document.getElementById(id);
   return msgElm.children;
 }
 
@@ -13,13 +13,38 @@ function getActiveMessage(messages) {
 
 function setMessage(action = "increase") {
   const messages = getMessages();
+  const contextMessages = getMessages("Context");
   let messageIndex = getActiveMessage(messages);
   let oldIndex = messageIndex;
 
+  if (messageIndex === 0 && action === "decrease") {
+    return;
+  }
+  if (messageIndex === messages.length - 1 && action === "increase") {
+    return;
+  }
+
   messages[messageIndex].classList.remove("show");
+  contextMessages[messageIndex].classList.remove("show");
   messageIndex = action === "increase" ? messageIndex + 1 : messageIndex - 1;
+
   messageIndex = messageIndex % messages.length;
   messages[messageIndex].classList.add("show");
+  contextMessages[messageIndex].classList.add("show");
+
+  if (messageIndex === 0) {
+    document.getElementById("left-arrow").classList.add("hide");
+    return;
+  } else {
+    document.getElementById("left-arrow").classList.remove("hide");
+  }
+  if (messageIndex === messages.length - 1) {
+    document.getElementById("right-arrow").classList.add("hide");
+    return;
+  } else {
+    document.getElementById("right-arrow").classList.remove("hide");
+  }
+
   return [oldIndex, messageIndex];
 }
 
